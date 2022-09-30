66b7653e52fe8ac55df88c4b66e43ea3b0576b91
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-09-30 14:46:12 +0200

    CSV artifact can be downloaded

--- pipeline/databus.py
+++ pipeline/databus.py
@@ -1,11 +1,50 @@
 import json
+import os
+import pathlib
 import urllib.parse
-from typing import List
+from typing import List, Union
 
 import requests
 from SPARQLWrapper import JSON, SPARQLWrapper2
 
 DATABUS_ENDPOINT = "https://energy.databus.dbpedia.org/sparql"
+
+
+def download_artifact(artifact_file: str, filename: Union[pathlib.Path, str]):
+    if not artifact_file.endswith(".csv"):
+        raise NotImplementedError("Currently only CSV artifacts can be downloaded.")
+    with open(os.path.join(filename), "wb") as f, requests.get(
+        artifact_file, stream=True, timeout=90
+    ) as r:
+        for line in r.iter_lines():
+            f.write(line + "\n".encode())
+
+
+def get_artifact_file(artifact: str, version: str) -> str:
+    sparql = SPARQLWrapper2(DATABUS_ENDPOINT)
+    sparql.setReturnFormat(JSON)
+
+    sparql.setQuery(
+        f"""
+        PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX dcat:   <http://www.w3.org/ns/dcat#>
+        PREFIX dct:    <http://purl.org/dc/terms/>
+        PREFIX dcv: <http://dataid.dbpedia.org/ns/cv#>
+        PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
+        SELECT ?file WHERE
+        {{
+            GRAPH ?g
+            {{
+                ?dataset dataid:artifact <{artifact}> .
+                ?distribution <http://purl.org/dc/terms/hasVersion> '{version}' .
+                ?distribution dataid:file ?file .
+            }}
+        }}
+        """
+    )
+    result = sparql.query()
+    return result.bindings[0]["file"].value
 
 
 def get_latest_version_of_artifact(artifact: str) -> str:
--- tests/test_databus.py
+++ tests/test_databus.py
@@ -1,16 +1,19 @@
+import pathlib
+import tempfile
+
 from pipeline import databus
+
+EXAMPLE_ARTIFACT = "https://energy.databus.dbpedia.org/henhuy/OEP/rli_dibt_windzone"
+EXAMPLE_COLLECTION = "https://energy.databus.dbpedia.org/henhuy/collections/sedos_test"
+CSV_ARTIFACT = "https://energy.databus.dbpedia.org/henhuy/general/testartifact/2022-01-20/testartifact_type=randomData.csv"
 
 
 def test_version():
-    EXAMPLE_ARTIFACT = "https://energy.databus.dbpedia.org/henhuy/OEP/rli_dibt_windzone"
     version = databus.get_latest_version_of_artifact(EXAMPLE_ARTIFACT)
     assert version == "2022-06-24"
 
 
 def test_collections():
-    EXAMPLE_COLLECTION = (
-        "https://energy.databus.dbpedia.org/henhuy/collections/sedos_test"
-    )
     artifacts = databus.get_artifacts_from_collection(EXAMPLE_COLLECTION)
     assert len(artifacts) == 2
     assert (
@@ -20,3 +23,20 @@ def test_collections():
     assert (
         artifacts[1] == "https://energy.databus.dbpedia.org/henhuy/general/testartifact"
     )
+
+
+def test_artifact_file():
+    databus_file = databus.get_artifact_file(EXAMPLE_ARTIFACT, "2022-06-24")
+    assert (
+        databus_file
+        == "https://energy.databus.dbpedia.org/henhuy/OEP/rli_dibt_windzone/2022-06-24/rli_dibt_windzone_.json"
+    )
+
+
+def test_download_csv():
+    test_filename = "test.csv"
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        csv_filename = pathlib.Path(tmpdirname) / test_filename
+        assert not csv_filename.exists()
+        databus.download_artifact(CSV_ARTIFACT, csv_filename)
+        assert csv_filename.exists()
